2ae1608ad116c51b396b22209bce2f4f2d57cc11
jeniwen <wang.jiawen1@live.ca>
2020-01-11 01:12:51 -0500

    Basic UI and Minimap!
    Added Timer and Bar scripts, two prefabs for UI, and JenTestScene, and some base images

---
+++ McGameJam-2020/Assets/Scripts/UI/Bar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    // private float barFull = 1.0f;
+    public int max;
+    private int currVal;
+    public Image barImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currVal = max;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeBarHealth(-1);
+        if (currVal < 0) 
+            currVal = max;
+        barImage.fillAmount = currVal * 1.0f / max;
+        
+    }
+
+    public void changeBarHealth(int amt) {
+        currVal += amt;
+    }
+}
