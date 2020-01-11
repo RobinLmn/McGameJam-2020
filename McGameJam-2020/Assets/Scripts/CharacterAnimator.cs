373a537ccf8c151c988d20bf9fc823f54084a25a
Mohamed Mohamed <mohamed.mohamed5@mail.mcgill.ca>
2020-01-11 08:27:06 -0500

    added a controller:)

---
+++ McGameJam-2020/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    public float animationSmooth = 0.1f;
+    NavMeshAgent agent;
+    Animator animator;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, animationSmooth, Time.deltaTime);
+    }
+}
--- McGameJam-2020/Assets/Scripts/PlayerController.cs
+++ McGameJam-2020/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     // public GameObject player;
     //PlayerMotor motor;
     Camera mainCam;
+    private bool platform = true;
+    public float rotatingSpeed;
 
     void Start()
     {
@@ -19,11 +21,52 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horInput = Input.GetAxis("Horizontal");
-        float verInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horInput, 0f, verInput);
-        Vector3 moveDestination = transform.position + movement;
-        GetComponent<NavMeshAgent>().destination = moveDestination;
+
+        if (platform==true) {
+            float horInput = Input.GetAxis("Horizontal");
+            float verInput = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(horInput, 0f, verInput);
+            Vector3 moveDestination = transform.position + movement;
+            GetComponent<NavMeshAgent>().destination = moveDestination;
+        }
+        else//platform is set to false
+        {
+
+
+            float horInput = Input.GetAxis("Horizontal");
+            float verInput = Input.GetAxis("Vertical");
+            //Mathf.Clamp(verInput, 0f, 1f);
+
+
+            Vector3 movement = transform.right * horInput + transform.forward * verInput ;//transform.right* horInput+ transform.forward*verInput;
+            Vector3 moveDestination = transform.position + movement;
+            GetComponent<NavMeshAgent>().destination = moveDestination;
+            transform.Rotate(Vector3.up* horInput *rotatingSpeed*Time.deltaTime);
+
+        }
+    }
+
+    public void SetPlatform(bool boo)
+    {
+        if (boo == true)
+        {
+            platform = true;
+        }
+        else {
+            platform = false;
+        }
+    }
+
+    public void SwitchPlatform()
+    {
+        if (platform == true)
+        {
+            platform = false;
+        }
+        else
+        {
+            platform = true;
+        }
     }
 
 
