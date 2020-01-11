8072917c937abe5d9453b34d7c6a2104cca8a27f
RENCUREL <pierre.rencurel@orange.fr>
2020-01-11 14:51:41 -0500

    Setting up day night cycle
    DONE : Setup day night cycle variables
    TODO : Implement and test

---
+++ McGameJam-2020/Assets/Scripts/DayNightCycle/DayNightCycle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    #region Variables
+    public bool pause = false;
+
+    [Header("Time")]
+    [Tooltip("Day Length in minutes")]
+    [SerializeField]
+    private float g_targetDayLength = 0.5f; //Length of the day
+    public float dayLength
+    {
+        get
+        {
+            return g_targetDayLength;
+        }
+    }
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float g_timeOfDay;
+    public float time
+    {
+        get
+        {
+            return g_timeOfDay;
+        }
+    }
+    #endregion
+
+    #region Methods
+
+
+    #endregion
+
+}
