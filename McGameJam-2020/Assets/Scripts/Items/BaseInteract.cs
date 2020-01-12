01255ad0f9aa681456924163f7fcabba60a335d4
Mohamed Mohamed <mohamed.mohamed5@mail.mcgill.ca>
2020-01-11 23:52:02 -0500

    .. what is this but okay

---
+++ McGameJam-2020/Assets/Scripts/Items/BaseInteract.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseInteract : Interactable
+{
+    #region Variables
+
+    GameManager gm;
+
+
+    #endregion
+
+    #region Base Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+
+    #endregion
+
+    #region Custom Methods
+
+    public override void Interact()
+    {
+        gm.g_gameEnd = true;
+        base.Interact();
+    }
+
+    #endregion
+}
---
+++ McGameJam-2020/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int oxStorage = 100;
+    public float oxLevel;
+    private float oxLossRate = 1;
+    private int sprintLossRate = 2;
+    PlayerController controller;
+
+    public Bar ox_bar;
+
+    private GameManager gm;
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void Start()
+    {
+        ox_bar.max = oxStorage;
+        gm = FindObjectOfType<GameManager>();
+        oxLevel = oxStorage;
+    }
+
+    private void FixedUpdate()
+    {
+        if (oxLevel >= 0 && controller.sprint == false)
+        {
+            oxLevel -= oxLossRate * Time.deltaTime;
+            ox_bar.currVal = oxLevel;
+        }
+        else if (oxLevel >= 0 && controller.sprint == true)
+        {
+            oxLevel -= sprintLossRate * Time.deltaTime;
+            ox_bar.currVal = oxLevel;
+        }
+        else
+        {
+            Die();
+        }
+    }
+
+    private void Update()
+    {
+       if (oxLevel > oxStorage)
+        {
+            Debug.Log("Maximum Oxygen Capacity");
+            oxLevel = oxStorage;
+        }
+    }
+
+    public void Die()
+    {
+        gm.g_playerDead = true;
+        Debug.Log("Player has died");
+    }
+}
