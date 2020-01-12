4488e0193b565240277dfa4559bcd1584da86350
Mohamed Mohamed <mohamed.mohamed5@mail.mcgill.ca>
2020-01-12 01:15:20 -0500

    lost files:(

---
+++ McGameJam-2020/Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public IEnumerator Shake(float time,float intensity)
+   {
+       Vector3 originalPos = transform.localPosition;
+       float elapsedTime = 0f;
+
+       while (elapsedTime < time)
+       {
+           float x = Random.Range(-1, 1) * intensity;
+           float y = Random.Range(-1, 1) * intensity;
+
+           transform.localPosition = new Vector3(x, y, x);
+           elapsedTime += Time.deltaTime;
+
+           yield return null;
+       }
+
+       transform.localPosition = originalPos;
+
+   }
+
+}
