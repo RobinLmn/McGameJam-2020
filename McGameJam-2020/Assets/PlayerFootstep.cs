a7c1efbaaf43486d047213a4d7660a46bd4d1179
yichenemma <56324983+yichenemma@users.noreply.github.com>
2020-01-12 10:52:54 -0500

    pull

---
+++ McGameJam-2020/Assets/PlayerFootstep.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFoorstep : MonoBehaviour
+{
+	private AudioSource footstep_Sound;
+    [SerializeField]
+	private AudioClip[] footstep_Clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
